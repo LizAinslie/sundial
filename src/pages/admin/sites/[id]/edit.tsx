5c1dcb8dd3acec72a89d512a7cf9afe8d70eba8e
Liz Ainslie <lizainslie16@gmail.com>
2023-02-18 19:53:02 -0700

    feat(admin): scaffold edit site page

---
+++ src/pages/admin/sites/[id]/edit.tsx
@@ -0,0 +1,70 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Site } from "@prisma/client";
+import { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import AdminHeader from "../../../../components/AdminHeader";
+import PageLayout from "../../../../components/PageLayout";
+import { getServerAuthSession } from "../../../../server/auth";
+import { prisma } from "../../../../server/db";
+
+type EditSitePageProps = {
+  site: Site;
+};
+
+const EditSitePage: NextPage<EditSitePageProps> = ({ site: serverSite }) => {
+  const [site, setSite] = useState(serverSite);
+
+  return (
+    <>
+      <Head>
+        <title>Edit Site - Sundial Admin</title>
+        <meta name="theme-color" content="#bae6fd" />
+      </Head>
+      <PageLayout>
+        <AdminHeader>
+          <div className="flex flex-row gap-4">
+            <Link href='/admin/users' className="bg-sky-500 hover:bg-sky-600 text-white rounded-md p-3 flex flex-row items-center gap-2">
+              <FontAwesomeIcon fixedWidth icon={faArrowLeft} />
+            </Link>
+
+            <div className="flex flex-col flex-grow">
+              <span className="font-bold">Editing {site.name}{(!site.enabled) && ' (disabled)'}</span>
+              <span className="text-sm">{site.id}</span>
+            </div>
+          </div>
+        </AdminHeader>
+      </PageLayout>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<EditSitePageProps> = async ({ params, req, res }) => {
+  // admins only
+  const session = await getServerAuthSession({ req, res });
+  if (!session?.user.admin) return {
+    redirect: {
+      destination: '/',
+      permanent: false,
+    },
+  };
+
+  const siteId = params!.id as string;
+
+  // look user up in db
+  const site = await prisma.site.findUnique({ where: { id: siteId } });
+  if (!site) return {
+    notFound: true,
+  };
+
+  return {
+    props: {
+      site,
+    },
+  };
+};
+
+export default EditSitePage;
+
